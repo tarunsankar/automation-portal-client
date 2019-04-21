e4360bd945f0682f935bca478651d1e7f24235ff
Sankar Anbazhgan <sankar84@gmail.com>
2019-04-21 05:36:32 -0700

    Changes for Props and State

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,32 @@
 import React from 'react'
 
 const Card = (props) => {
-  return (
 
-    <div className="col-md-4">
-        <div className="card card-chart">
-            <div className="card-header card-header-success">
-                <h4 className="card-title">Workday</h4>
-                <p className="card-category"></p>
-            </div>
-            <div className="card-body">
-                <h4 className="card-title">Test Automation</h4>
-                <p className="card-category">
-                <span className="text-success"><i className="fa fa-long-arrow-up"></i> 55% </span> increase in passed test cases.</p>
-            </div>
-            <div className="card-footer">
-                <div className="stats">
-                    <i className="material-icons">access_time</i> updated 4 minutes ago
+  const { cards } = props;
+  const cardsData = cards.map(card => {
+      return (
+        <div className="col-md-4" key={card.id}>
+            <div className="card card-chart">
+                <div className={card.titleClass}>
+                    <h4 className="card-title">{card.heading}</h4>
+                    <p className="card-category"></p>
+                </div>
+                <div className="card-body">
+                    <h4 className="card-title">{card.subheading}</h4>
+                    <p className="card-category">
+                    <span className={card.status}><i className="fa fa-long-arrow-up"></i> {card.score}% </span> {card.statusText} in passed test cases.</p>
+                </div>
+                <div className="card-footer">
+                    <div className="stats">
+                        <i className="material-icons">access_time</i> updated {card.time} ago
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+      )
+  })
 
-  )
+  return (<div className='row justify-content-md-center h-100'>{cardsData}</div>)
 }
 
 export default Card
